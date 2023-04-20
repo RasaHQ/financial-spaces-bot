688db349654ecc3a4d0e7c87f7926c17b0f18a09
Varun Shankar S <varunshankar55@gmail.com>
2023-04-20 14:36:46 +0530

    actions update

--- investment/actions/actions.py
+++ investment/actions/actions.py
@@ -6,17 +6,26 @@ class ActionDoInvestment(Action):
         return "action_do_investment"
 
     def run(self, dispatcher, tracker, domain):
-        investment_type = tracker.get_slot("investment_type")
-        if investment_type == "stocks":
-            dispatcher.utter_message("You are interested in stocks")
-        elif investment_type == "bonds":
-            dispatcher.utter_message("You are interested in bonds")
-        elif investment_type == "mutual funds":
-            dispatcher.utter_message("You are interested in mutual funds")
-        elif investment_type == "real estate":
-            dispatcher.utter_message("You are interested in real estate")
-        elif investment_type == "cryptocurrencies":
-            dispatcher.utter_message("You are interested in cryptocurrencies")
-        else:
-            dispatcher.utter_message("You are interested in something else")
+        for investment_type in tracker.get_latest_entity_values("investment_type"):
+            if investment_type == "stocks":
+                dispatcher.utter_message("You are interested in stocks")
+            elif investment_type == "bonds":
+                dispatcher.utter_message("You are interested in bonds")
+            elif investment_type == "mutual funds":
+                dispatcher.utter_message("You are interested in mutual funds")
+            elif investment_type == "real estate":
+                dispatcher.utter_message("You are interested in real estate")
+            elif investment_type == "cryptocurrencies":
+                dispatcher.utter_message("You are interested in cryptocurrencies")
+            else:
+                dispatcher.utter_message("You are interested in something else")
         return [SlotSet("display_options", True)]
+
+
+class ActionShowStatus(Action):
+    def name(self):
+        return "action_show_status"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(response= "utter_financial_status", name = "Varun")
+        return
